b99e9edcc9e13f80c15d730356fd7d07e5e6618b
MisterKingbad <davymichael20@gmail.com>
2023-06-14 15:48:20 -0300

    feat: :sparkles: mod

--- 4-bancoDeDados/1/main.go
+++ 4-bancoDeDados/1/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/google/uuid"
 )
 
@@ -19,5 +21,9 @@ func NewProduct(name string, price float64) *Product {
 }
 
 func main() {
-
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err !=  nil {
+		panic(err)
+	}
+	defer db.Close()
 }
